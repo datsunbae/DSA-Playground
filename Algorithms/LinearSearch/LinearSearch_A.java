9208f399a96e698d536984b431ef9554e7e1ea74
kodoku-x <datsunbae@gmail.com>
2023-07-05 05:22:39 +0700

    :hammer: Linear Search

---
+++ Algorithms/LinearSearch/LinearSearch_A.java
@@ -0,0 +1,25 @@
+package Algorithms.LinearSearch;
+
+public class LinearSearch_A {
+    public static void main(String[] args) {
+        int[] a = new int[] { 2, 6, 1, 8, 9, 12, 14, 3 };
+
+        int result = linearSearch(a, 11111);
+
+        if (result != -1) {
+            System.out.println("Index: " + result);
+        } else {
+            System.out.println("Not found");
+        }
+
+    }
+
+    private static int linearSearch(int[] a, int value) {
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == value)
+                return i;
+        }
+        return -1;
+    }
+}
